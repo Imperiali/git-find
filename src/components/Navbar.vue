62d4dd0f3f2f4a1b1614a2e802244e8aa2acc48b
Igor Imperiali <ifernandes@estantevirtual.com.br>
2019-08-13 20:48:00 -0300

    Creating Navbar component

---
+++ src/components/Navbar.vue
@@ -0,0 +1,15 @@
+<template>
+    <nav class="navbar navbar-light bg-light">
+        <a href="/" class="navbar-brand">Git Find</a>
+    </nav>
+</template>
+
+<script>
+  export default {
+    name: "Navbar"
+  }
+</script>
+
+<style scoped>
+
+</style>
