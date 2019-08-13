bd87e36df220fb10c7a4cbef1fe971afe5de8aa2
Igor Imperiali <ifernandes@estantevirtual.com.br>
2019-08-13 20:47:51 -0300

    Creating Input component

---
+++ src/components/Input.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="input-group mb-3">
+        <div class="input-group-prepend">
+            <span class="input-group-text" id="addon-wrapping">@</span>
+        </div>
+        <input type="text" class="form-control" placeholder="Github's username"
+               aria-label="Github's username" aria-describedby="button-addon2">
+        <div class="input-group-append">
+            <button class="btn btn-outline-secondary" type="button" id="button-addon2">Search</button>
+        </div>
+    </div>
+</template>
+
+<script>
+  export default {
+    name: "Input"
+  }
+</script>
+
+<style scoped>
+
+</style>
