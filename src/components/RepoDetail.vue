58202e9c18c19f060011beb293ef363c72689737
Igor Imperiali <ifernandes@estantevirtual.com.br>
2019-08-14 09:10:24 -0300

    Adding repositories details component

---
+++ src/components/RepoDetail.vue
@@ -0,0 +1,23 @@
+<template>
+    <ul class="list-group">
+        <li class="list-group-item d-flex justify-content-between align-items-center">
+            <ul class="list-group list-group-horizontal-md">
+                <li class="list-group-item"><a :href="repo.url" class=""> {{repo.name}} </a></li>
+                <li class="list-group-item">{{repo.private}}</li>
+                <li class="list-group-item">{{repo.description}}</li>
+            </ul>
+            <span class="badge badge-primary badge-pill">{{repo.stargazers_count}}</span>
+        </li>
+    </ul>
+</template>
+
+<script>
+  export default {
+    name: "RepoDetail",
+    props: ['repo']
+  }
+</script>
+
+<style scoped>
+
+</style>
