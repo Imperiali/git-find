412a5704f7cfa431e2301919a0bd27bbb0839e99
Igor Imperiali <ifernandes@estantevirtual.com.br>
2019-08-14 18:59:22 -0300

    Changing url to correct url

--- src/components/RepoDetail.vue
+++ src/components/RepoDetail.vue
@@ -2,7 +2,7 @@
     <ul class="list-group">
         <li class="list-group-item d-flex justify-content-between align-items-center">
             <ul class="list-group list-group-horizontal-md">
-                <li class="list-group-item"><a :href="repo.url" class=""> {{repo.name}} </a></li>
+                <li class="list-group-item"><a target="_blank" :href="repo.html_url" class=""> {{repo.name}} </a></li>
                 <li class="list-group-item" v-if="repo.description">{{repo.description}}</li>
             </ul>
             <span class="badge badge-primary badge-pill">{{repo.stargazers_count}}</span>
