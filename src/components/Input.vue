28b80cf446a62d5c0ab3dc9e7fe0c7584780074a
Igor Imperiali <ifernandes@estantevirtual.com.br>
2019-08-14 09:11:30 -0300

    Encapsulating in form

--- src/components/Input.vue
+++ src/components/Input.vue
@@ -1,14 +1,19 @@
 <template>
-    <div class="input-group mb-3">
-        <div class="input-group-prepend">
-            <span class="input-group-text" id="addon-wrapping">@</span>
+    <form @submit.prevent="fetchUser(userLogin)">
+        <div class="input-group mb-3">
+            <div class="input-group-prepend">
+                <span class="input-group-text" id="addon-wrapping">@</span>
+            </div>
+            <input type="text" class="form-control" placeholder="Github's username"
+                   aria-label="Github's username" aria-describedby="button-addon2"
+                   v-model="userLogin">
+            <div class="input-group-append">
+                <button @click="fetchUser(userLogin)" class="btn btn-outline-secondary" type="button"
+                        id="button-addon2">Search
+                </button>
+            </div>
         </div>
-        <input type="text" class="form-control" placeholder="Github's username"
-               aria-label="Github's username" aria-describedby="button-addon2">
-        <div class="input-group-append">
-            <button class="btn btn-outline-secondary" type="button" id="button-addon2">Search</button>
-        </div>
-    </div>
+    </form>
 </template>
 
 <script>
