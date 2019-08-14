34945341a77529ba92408cf7936d1fa19f7f4dcc
Igor Imperiali <ifernandes@estantevirtual.com.br>
2019-08-14 09:10:48 -0300

    Adding no user component

---
+++ src/components/NoUser.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="card">
+        <div class="card-body">
+            No user find yet...
+        </div>
+    </div>
+</template>
+
+<script>
+  export default {
+    name: "NoUser"
+  }
+</script>
+
+<style scoped>
+
+</style>
