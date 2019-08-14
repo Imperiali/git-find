181f0442ed02059bfbbe79178204ce4a8434a2af
Igor Imperiali <ifernandes@estantevirtual.com.br>
2019-08-13 21:00:43 -0300

    Using mocked dada

--- src/components/UserCard.vue
+++ src/components/UserCard.vue
@@ -1,17 +1,19 @@
 <template>
     <div class="card">
         <div class="card-header">
-            Featured
+            {{user.login}}
         </div>
         <div class="card-body">
             <div class="row">
                 <div class="col-lg-3 col-md-5 col-sm-12">
-                    <img src="../assets/logo.png" class="" alt="">
+                    <img :src="user.avatar_url" class="img-fluid" alt="">
                 </div>
                 <div class="col-lg-9 col-md-7 col-sm-12">
-                    <h5 class="card-title">Special title treatment</h5>
-                    <p class="card-text">With supporting text below as a natural lead-in to additional
-                        content.</p>
+                    <h5 v-if="user.email" class="card-title">{{user.email}}</h5>
+                    <h5 v-else class="card-title">No e-mail avaliable :(</h5>
+                    <span class="badge badge-secondary">{{user.followers}} Followers</span> |
+                    <span class="badge badge-secondary">Following {{user.following}}</span>
+                    <p class="card-text">{{user.bio}}</p>
                     <router-link btn btn-primary :to="user.login" tag="a">Go to repositories!</router-link>
                 </div>
             </div>
