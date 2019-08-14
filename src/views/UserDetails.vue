43d5aee4aca6b7ffb183695f232a9c14962a4cb8
Igor Imperiali <ifernandes@estantevirtual.com.br>
2019-08-13 21:12:17 -0300

    Reciving props by routing

---
+++ src/views/UserDetails.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="user-details">
+        {{login}} {{ followers }} {{ following}}{{ bio}} {{email}} {{avatar_url}}
+    </div>
+</template>
+
+<script>
+  export default {
+    name: "UserDetails",
+    props: ['login', 'followers', 'following', 'bio', 'email', 'avatar_url']
+  }
+</script>
+
+<style scoped>
+
+</style>
