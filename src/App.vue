0778ba74f2fab9c57cf6cfcb196eb9c778941385
Igor Imperiali <ifernandes@estantevirtual.com.br>
2019-08-14 18:58:42 -0300

    Changing to vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,17 @@
 <template>
-  <div id="app">
-    <Navbar/>
-    <div class="container">
-      <router-view/>
-    </div>
-  </div>
+    <v-app id="app">
+        <v-content>
+            <Navbar/>
+            <router-view/>
+        </v-content>
+    </v-app>
 </template>
 
 <style lang="scss">
 </style>
 <script>
   import Navbar from "./components/Navbar";
+
   export default {
     components: {Navbar}
   }
