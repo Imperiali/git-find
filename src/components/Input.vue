b61f7c5010513956ffd7c75df5bb1e328e898121
Igor Imperiali <ifernandes@estantevirtual.com.br>
2019-08-15 17:27:37 -0300

    Removing bootstrap

--- src/components/Input.vue
+++ src/components/Input.vue
@@ -1,18 +1,9 @@
 <template>
     <form @submit.prevent="fetchUser(userLogin)">
-        <div class="input-group mb-3">
-            <div class="input-group-prepend">
-                <span class="input-group-text" id="addon-wrapping">@</span>
-            </div>
-            <input type="text" class="form-control" placeholder="Github's username"
-                   aria-label="Github's username" aria-describedby="button-addon2"
-                   v-model="userLogin">
-            <div class="input-group-append">
-                <button @click="fetchUser(userLogin)" class="btn btn-outline-secondary" type="button"
-                        id="button-addon2">Search
-                </button>
-            </div>
-        </div>
+        <v-text-field v-model="userLogin">
+            <v-icon slot="prepend" color="green">@</v-icon>
+            <v-btn @click="fetchUser(userLogin)" slot="append" outlined>Search</v-btn>
+        </v-text-field>
     </form>
 </template>
 
