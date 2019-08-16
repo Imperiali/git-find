967945ece16e6d0f3f68a05e94dd5825b09f440d
Igor Imperiali <ifernandes@estantevirtual.com.br>
2019-08-15 23:38:19 -0300

    Starting atomic design

---
+++ src/components/atoms/GF-link/index.vue
@@ -0,0 +1,18 @@
+<template>
+    <a :href="href" class="green--text">
+        <slot></slot>
+    </a>
+</template>
+
+<script>
+  export default {
+    name: "GF-link",
+    props: ['href']
+  }
+</script>
+
+<style scoped>
+    a {
+        text-decoration: none;
+    }
+</style>
