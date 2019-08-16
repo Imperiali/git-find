b079d68e50fed15e7a6f512a94f3c2ffa799e29c
Igor Imperiali <ifernandes@estantevirtual.com.br>
2019-08-16 09:00:33 -0300

    Adding color to loading atom

--- src/components/atoms/GF-Loading.vue
+++ src/components/atoms/GF-Loading.vue
@@ -3,14 +3,15 @@
         <v-progress-circular
                 indeterminate
                 size="120"
-                color="light-blue"
+                :color="color"
         ></v-progress-circular>
     </v-row>
 </template>
 
 <script>
   export default {
-    name: "GF-Loading"
+    name: "GF-Loading",
+    props:['color']
   }
 </script>
 
