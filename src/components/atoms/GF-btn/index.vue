735a02e38fa4b9bdbe0118cec7b3d6a781fa0c8c
Igor Imperiali <ifernandes@estantevirtual.com.br>
2019-08-15 23:37:44 -0300

    Starting atomic design

---
+++ src/components/atoms/GF-btn/index.vue
@@ -0,0 +1,19 @@
+<template>
+    <v-btn @click="$emit('click')"
+           :color="color"
+           :outlined="outlined"
+           :slot="append">
+        <slot></slot>
+    </v-btn>
+</template>
+
+<script>
+  export default {
+    name: "GF-btn",
+    props: ['color', 'outlined', 'append']
+  }
+</script>
+
+<style scoped>
+
+</style>
