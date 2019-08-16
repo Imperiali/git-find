cf132311c08d97776cec6ac59e72cab8b21c9305
Igor Imperiali <ifernandes@estantevirtual.com.br>
2019-08-16 18:51:12 -0300

    Configuring jest to look to store's tests

--- jest.config.js
+++ jest.config.js
@@ -21,7 +21,8 @@ module.exports = {
   ],
   testMatch: [
     '**/tests/unit/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx)',
-    '**/src/components/**/**/*.spec.(js|jsx|ts|tsx)'
+    '**/src/components/**/**/*.spec.(js|jsx|ts|tsx)',
+    '**/src/**/**/*.spec.(js|jsx|ts|tsx)'
   ],
   testURL: 'http://localhost/',
   watchPlugins: [
