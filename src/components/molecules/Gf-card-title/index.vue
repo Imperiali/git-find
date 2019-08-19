ced1785f16867b69931428a6f410b7e879b6d2a8
Igor Imperiali <ifernandes@estantevirtual.com.br>
2019-08-19 08:14:55 -0300

    Adding molecule gf card title

---
+++ src/components/molecules/Gf-card-title/index.vue
@@ -0,0 +1,21 @@
+<template>
+    <v-card-title class="headline mb-1">
+        <v-layout>
+            <slot name="title"></slot>
+        </v-layout>
+        <v-layout class="justify-end">
+            <slot name="extra"></slot>
+        </v-layout>
+    </v-card-title>
+</template>
+
+<script>
+  export default {
+    name: "GF-card-title",
+    props: ['title']
+  }
+</script>
+
+<style scoped>
+
+</style>
